afdfc464dd499aece4d5c76757f8032e0a6af51d
Devin Scholl <devinscholl1@gmail.com>
2019-03-03 04:45:13 -0800

    Cleaned up logic

--- message_sms_sender.js
+++ message_sms_sender.js
@@ -9,62 +9,25 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*function message(body1, from1, to1)
-{
-  return client.messages
-  .create({
-     body: body1 || 'Hello there',
-     from: from1 || '+19168239140',
-     to: to1 || '+19166178309'
-   })
-}*/
 
-function getSID(message)
-{
-  return message.sid;
-}
-
-function processIncomingSMS(message, receiver, sender) {
-  logic.IncomingRawSMS(message, receiver, sender);
-}
-
-exports.sendOutgoingSMS = 
+exports.sendOutgoingSMS =
 function sendOutgoingSMS(message, receiver, sender) {
-  app.post("/message", (req, res) => {
     client.messages
       .create({
         body: message,
         from: sender,
         to: receiver
       })
-      .then(message => console.log(message.sid));
-  })
-}
+    .then(message => console.log(message.sid))
+    .catch(err => console.log(err));
+};
 
 app.post('/sms', (req, res) => {
-  //console.log(req.body.Body);	//gets the message of sender
-  //console.log(req.body.From);	//gets the phone number of sender
-
-  processIncomingSMS(req.body.Body, req.body.To, req.body.From);
-
-  //message(req.body.Body, '+19168239140', '+19168350353');	//sends the message
-  
+  logic.IncomingRawSMS(req.body.Body, req.body.To, req.body.From);
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end('');
 });
 
-
-
-
-app.post("/message", (req, res) => {
-  message("Testing /message endpoint")
-  .then(message => {
-    res.send(message.sid);
-  })
-  .catch(err => {
-    res.sendStatus(500);
-  });
-});
 
 app.listen(3000, () => {
   console.log("Server started!");
