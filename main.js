6cc4a3cd5352a6e6acdea55a73954fc5068bde1f
Devin Scholl <devinscholl1@gmail.com>
2019-03-02 18:22:35 -0800

    Added gcp proxy support and changed default port to 8080

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.enable("trust proxy");
+
 app.get('/', (req, res) => {
   res.send('Hello Express app!');
 });
 
-app.listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('server started');
 });
